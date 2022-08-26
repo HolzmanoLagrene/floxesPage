a2e04dc4318682ad886afda6997bbabfa2b3fbf1
Lenz Baumann <holzmano@Lenzs-MBP.fritz.box>
2022-08-27 00:01:06 +0200

    Fixing up for prod

--- start_production.sh
+++ start_production.sh
@@ -1,3 +1,5 @@
+sudo pkill -9 nginx
+sudo nginx -c /home/analyst/floxesPage/nginx/nginx.conf
 docker compose down
 docker compose up -d --build
 docker compose exec web python manage.py migrate --noinput
