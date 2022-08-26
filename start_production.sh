2512dd05720aee5ba7a65c758d7c112a45182bd0
Lenz Baumann <holzmano@Lenzs-MBP.fritz.box>
2022-08-27 00:16:33 +0200

    another fix

--- start_production.sh
+++ start_production.sh
@@ -1,7 +1,11 @@
-sudo pkill -9 nginx
-sudo nginx -c /home/analyst/floxesPage/nginx/nginx.conf
 docker compose down
 docker compose up -d --build
 docker compose exec web python manage.py migrate --noinput
 docker compose exec web python manage.py collectstatic --no-input --clear
+
+sudo pkill -9 nginx
+sudo nginx -c /home/analyst/floxesPage/nginx/nginx.conf
+sudo chown -R analyst:www-data /home/analyst/floxesPage/staticfiles
+sudo chmod -R 750 /home/analyst/floxesPage/staticfiles
+
 docker compose logs -f
